03d2991f6c67b02e7ccf8e3594bb578761da82b1
LINAD <andreas.linden@coop.ch>
2018-03-28 14:16:05 +0200

    Cleand some spaces

--- runners/elasticreactor_new.py
+++ runners/elasticreactor_new.py
@@ -197,48 +197,48 @@ def log_stuff(data_str):
   return True
 
 def WriteToEs(data, casetype, change_count, error_count, payload):
- '''
- Write the gathered data to an elasticsearch server
- '''
- config = _get_elasticreator_configuration()
- utc = UTC()
- master = os.uname()[1]
- minion_name = data['id']
- job_fun = data['fun']
- job_id = data['jid']
+  '''
+  Write the gathered data to an elasticsearch server
+  '''
+  config = _get_elasticreator_configuration()
+  utc = UTC()
+  master = os.uname()[1]
+  minion_name = data['id']
+  job_fun = data['fun']
+  job_id = data['jid']
 
- es_data = {}
+  es_data = {}
 
- es_data['@timestamp'] = datetime.datetime.now(utc).isoformat()
- es_data['case'] = casetype
- es_data['change_count'] = change_count
- es_data['error_count'] = error_count
- es_data['minion'] = minion_name
- es_data['master'] = master
- es_data['fun'] = job_fun
- es_data['jid'] = job_id
- es_data['data'] = payload
+  es_data['@timestamp'] = datetime.datetime.now(utc).isoformat()
+  es_data['case'] = casetype
+  es_data['change_count'] = change_count
+  es_data['error_count'] = error_count
+  es_data['minion'] = minion_name
+  es_data['master'] = master
+  es_data['fun'] = job_fun
+  es_data['jid'] = job_id
+  es_data['data'] = payload
 
- es_host = config['es_host']
- es_port = config['es_port']
- es_index = config['es_index']
- es_index_date = config['es_index_date']
- es_doc_type= config['es_doc_type']
+  es_host = config['es_host']
+  es_port = config['es_port']
+  es_index = config['es_index']
+  es_index_date = config['es_index_date']
+  es_doc_type= config['es_doc_type']
 
- if es_index_date:
+  if es_index_date:
     es_index = '{0}-{1}'.format(es_index, datetime.date.today().strftime('%Y.%m.%d')) 
 
- es = Elasticsearch([{'host': es_host,'port': es_port}])
+  es = Elasticsearch([{'host': es_host,'port': es_port}])
 
- try:
-   es.index(index=es_index, doc_type=es_doc_type, body=json.dumps(es_data))
+  try:
+    es.index(index=es_index, doc_type=es_doc_type, body=json.dumps(es_data))
 
- except TransportError as e:
-  # ignore already existing index
-  if e.error == 'index_already_exists_exception':
-    pass
-  else:
-    raise
+  except TransportError as e:
+    # ignore already existing index
+    if e.error == 'index_already_exists_exception':
+      pass
+    else:
+      raise
 
   #log.debug('es_host: ' + es_host + ' es_port: ' + es_port + ' es_index: ' + es_index + ' es_doc_type: ' + es_doc_type)
   return True
