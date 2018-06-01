4c5a87b92d495571637349a0e93748aa90bef172
Andreas Linden <andreas.linden@coop.ch>
2018-06-01 08:02:03 +0200

    Add script to test a logstash instance

---
+++ scripts/test_logstash.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from datetime import tzinfo, timedelta
+
+import subprocess
+import datetime
+import json
+import os
+import socket
+
+class UTC(tzinfo):
+  def utcoffset(self, dt):
+    return timedelta(0)
+
+  def tzname(self, dt):
+    return 'UTC'
+
+  def dst(self, dt):
+    return timedelta(0)
+
+ls_data = {
+    '@timestamp': datetime.datetime.now(utc).isoformat(),
+    "fqdn": "linadlinad.linad.ch",
+    "message": 'hello world',
+    "type": "aix_syslogs",
+  }
+
+ls_host = 'svrl1esmlsp01.hs.coop.ch'
+ls_port = 10666
+
+utc = UTC()
+try:
+#  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+except socket.error as e:
+  raise
+
+
+try:
+  sock.connect((ls_host, ls_port))
+except socket.error as e:
+  raise
+
+sock.send(json.dumps(ls_data))
+sock.close()
